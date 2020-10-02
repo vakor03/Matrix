1e68a4b86ce58d613bc983cab1ddf7ba85f34fe1
vakor03 <myksv200@gmail.com>
2020-10-02 16:02:34 +0300

    Add sum of Matrix, Set output of functions to Matrix type Add Setters/Getters

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Matrix m1 = new Matrix();
         Scanner sc = new Scanner(System.in);
+        Matrix m1 = new Matrix();
         m1.InitializeMatrix(sc);
-        m1.PrintMatrix();
+       // int[][] K = m1.MultipleWithNumber(2);
+       // int[][] H = m1.GetMatrix();
+       // m1.PrintMatrix(K);
+       // m1.PrintMatrix(H);
 
+        Matrix m2 = new Matrix();
+        m2.InitializeMatrix(sc);
+        //int[][] H2 = m2.GetMatrix();
+        //m2.PrintMatrix(H2);
+        Matrix Sum = m1.SumOfMatrix(m2);
+        PrintMatrix(Sum);
+        System.out.println();
+    }
+    public static void PrintMatrix(Matrix RandomM){
+        for (int i = 0; i < RandomM.getHeightOfMatrix(); i++) {
+            System.out.println(Arrays.toString(RandomM.getM()[i]));
+        }
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Matrix {
@@ -8,17 +6,45 @@ public class Matrix {
     private int heightOfMatrix;
     private int[][] M;
 
+    public int[][] getM() {
+        return M;
+    }
+
+    public int getHeightOfMatrix() {
+        return heightOfMatrix;
+    }
+
+    public void setWidthOfMatrix(int widthOfMatrix) {
+        this.widthOfMatrix = widthOfMatrix;
+    }
+
+    public void setHeightOfMatrix(int heightOfMatrix) {
+        this.heightOfMatrix = heightOfMatrix;
+    }
+
+    public void setM(int[][] m) {
+        M = m;
+    }
+
+    public int getWidthOfMatrix() {
+        return widthOfMatrix;
+    }
 
     protected void InitializeMatrix(Scanner sc){
         int[] WH = StrMIntoIntM(sc.next().split(","),2);
         widthOfMatrix = WH[0];
         heightOfMatrix = WH[1];
-        M = new int[widthOfMatrix][heightOfMatrix];
+        M = new int[heightOfMatrix][widthOfMatrix];
         for (int i = 0 ;i<heightOfMatrix; i++){
             String[] str1 = sc.next().split(",");
             M[i] = (StrMIntoIntM(str1,widthOfMatrix));
 
         }
+
+    }
+
+    protected int[][] GetMatrix(){
+        return M;
     }
 
     private int[] StrMIntoIntM(String[] str, int width){
@@ -29,11 +55,35 @@ public class Matrix {
         return INT;
     }
 
-    protected void PrintMatrix(){
+
+
+    protected int[][] MultipleWithNumber(int number){
+        int[][] MWN = new int[heightOfMatrix][widthOfMatrix];
         for (int i = 0; i < heightOfMatrix; i++) {
-            System.out.println(Arrays.toString(M[i]));
+            for (int j = 0; j < widthOfMatrix; j++) {
+                MWN[i][j] = M[i][j] * number;
+            }
         }
+        return MWN;
     }
+
+    protected Matrix SumOfMatrix(Matrix m1){
+        int[][] SOM = new int[heightOfMatrix][widthOfMatrix];
+        if(m1.getHeightOfMatrix()==this.getHeightOfMatrix() && m1.getWidthOfMatrix()==this.getWidthOfMatrix()){
+            for (int i = 0; i < heightOfMatrix; i++) {
+                for (int j = 0; j < widthOfMatrix; j++) {
+                    SOM[i][j] = M[i][j] + m1.GetMatrix()[i][j];
+                }
+            }
+        }
+        Matrix som = new Matrix();
+        som.setM(SOM);
+        som.setHeightOfMatrix(heightOfMatrix);
+        som.setWidthOfMatrix(widthOfMatrix);
+        return som;
+    }
+
+
 
 
 }
